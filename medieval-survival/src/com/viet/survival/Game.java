61029c8277544635f5c2b91015a36976cfa85f8a
Viet <quangvietnguyen22@gmail.com>
2026-07-08 22:59:59 +0200

    Added several classes: - Game.java - Main.java - Village.java

---
+++ medieval-survival/src/com/viet/survival/Game.java
@@ -0,0 +1,12 @@
+package com.viet.survival;
+
+public class Game {
+
+    private Village village = new Village();
+
+    public void start() {
+        System.out.println("=== Medieval Survival ===");
+    }
+}
+
+
---
+++ medieval-survival/src/com/viet/survival/Village.java
@@ -0,0 +1,11 @@
+package com.viet.survival;
+
+
+public class Village {
+
+    private int food = 20;
+    private int wood = 10;
+    private int day = 1;
+
+}
+
