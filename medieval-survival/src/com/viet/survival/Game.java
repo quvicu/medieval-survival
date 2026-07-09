9e3da01699e4e05e2080a1db50a1c952c87f4ed6
Viet <quangvietnguyen22@gmail.com>
2026-07-09 14:46:33 +0200

    feat: add current village stats

--- medieval-survival/src/com/viet/survival/Game.java
+++ medieval-survival/src/com/viet/survival/Game.java
@@ -11,14 +11,13 @@ public class Game {
                     2. Gather Wood
                     3. End Day
                     4. Quit""";
+
     private final Scanner scanner = new Scanner(System.in);
 
     public void start() {
         do {
             System.out.println("=== Medieval Survival ===");
-            System.out.println("Day: " + village.getDay());
-            System.out.println("Food: " + village.getFood());
-            System.out.println("Wood: " + village.getWood());
+            printVillageStats();
             System.out.println(MENU);
             int menuChoice = scanner.nextInt();
             switch (menuChoice) {
@@ -38,16 +37,12 @@ public class Game {
         } while (isRunning);
     }
 
-    private void quit() {
-        isRunning = false;
-    }
-
 
     public Village getVillage() {
         return village;
     }
 
-    public void gatherFood(){
+    private void gatherFood(){
         int randomFoodAmount = (int)(Math.random() * 18);
         int currentFood = village.getFood();
         int totalFood = currentFood + randomFoodAmount;
@@ -56,7 +51,7 @@ public class Game {
         System.out.println("Gathered: " + randomFoodAmount + " Food\nTotal Food: " + totalFood);
     }
 
-    public void gatherWood(){
+    private void gatherWood(){
         int randomWoodAmount = (int)(Math.random() * 18);
         int currentWood = village.getWood();
         int totalWood = currentWood + randomWoodAmount;
@@ -65,9 +60,19 @@ public class Game {
         System.out.println("Gathered: " + randomWoodAmount + " Wood\nTotal Wood: " + totalWood);
     }
 
-    public void endDay(){
+    private void endDay(){
         village.setDay(village.getDay() + 1);
         System.out.println("Day " + village.getDay() + " begins.");
+    }
+
+    private void quit() {
+        isRunning = false;
+    }
+
+    private void printVillageStats(){
+        System.out.println("Day: " + village.getDay());
+        System.out.println("Food: " + village.getFood());
+        System.out.println("Wood: " + village.getWood());
     }
 }
 
