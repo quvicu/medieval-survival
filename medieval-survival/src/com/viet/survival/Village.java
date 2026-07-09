4646a0dbff1145719bb7d3f71ef6d2bae9c7334b
quvicu <quangvietnguyen22@gmail.com>
2026-07-09 02:31:09 +0200

    feat: implement village food resource management

--- medieval-survival/src/com/viet/survival/Village.java
+++ medieval-survival/src/com/viet/survival/Village.java
@@ -3,9 +3,37 @@ package com.viet.survival;
 
 public class Village {
 
-    private int food = 20;
-    private int wood = 10;
-    private int day = 1;
+    private int food;
+    private int wood;
+    private int day;
+
+    public Village(int food, int wood, int day) {
+        this.food = food;
+        this.wood = wood;
+        this.day = day;
+    }
+
+    public int getFood() {
+        return food;
+    }
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public int getWood() {
+        return wood;
+    }
+    public void setWood(int wood) {
+        this.wood = wood;
+    }
+
+    public int getDay() {
+        return day;
+    }
+    public void setDay(int day) {
+        this.day = day;
+    }
 
 }
+
 
