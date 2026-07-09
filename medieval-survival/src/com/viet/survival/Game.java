3a4df55fd2af7cbaee3d497326e6073be8d042d0
quvicu <quangvietnguyen22@gmail.com>
2026-07-09 02:31:33 +0200

    feat: implement village food resource management

--- medieval-survival/src/com/viet/survival/Game.java
+++ medieval-survival/src/com/viet/survival/Game.java
@@ -1,12 +1,26 @@
 package com.viet.survival;
 
 public class Game {
-
-    private Village village = new Village();
+    private final Village village = new Village(10, 10, 1);
 
     public void start() {
         System.out.println("=== Medieval Survival ===");
+
+    }
+
+    public Village getVillage() {
+        return village;
+    }
+
+    public void gatherFood(){
+        int gatheredFood = (int)(Math.random() * 18);
+        int currentFood = village.getFood();
+        int totalFood = currentFood + gatheredFood;
+
+        village.setFood(totalFood);
+        System.out.println("Gathered: " + gatheredFood + " Food\nTotal Food: " + totalFood);
     }
 }
+
 
 
--- medieval-survival/src/com/viet/survival/Main.java
+++ medieval-survival/src/com/viet/survival/Main.java
@@ -2,6 +2,15 @@ package com.viet.survival;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== Medieval Survival ===");
+        Game game = new Game();
+        game.start();
+
+        game.gatherFood();
+        game.gatherFood();
+        game.gatherFood();
+
+
+        System.out.println(game.getVillage().getFood());
+
     }
 }
