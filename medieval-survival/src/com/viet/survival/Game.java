633643b26a5a4da3105e2c3c59d66b56e874e766
Viet <quangvietnguyen22@gmail.com>
2026-07-17 20:08:10 +0200

    refactor: extract scholar functionality into separate class

--- medieval-survival/src/com/viet/survival/Game.java
+++ medieval-survival/src/com/viet/survival/Game.java
@@ -14,15 +14,9 @@ public class Game {
                     4. Talk to Scholar
                     5. Recruit Villager
                     6. Quit""";
-    private static final String SCHOLAR_MENU =
-            """
-                    1. Talk to Scholar
-                    2. Binary ->Decimal
-                    3. Decimal -> Binary
-                    4. Ask Scholar:
-                    5. Leave""";
 
     private final Scanner scanner = new Scanner(System.in);
+    private final Scholar scholar = new Scholar(scanner);
 
     public void start() {
         do {
@@ -41,7 +35,7 @@ public class Game {
                     endDay();
                     break;
                 case 4:
-                    talkToScholar();
+                    scholar.start();
                     break;
                 case 5:
                     recruitVillager();
@@ -83,73 +77,6 @@ public class Game {
 
         village.setDay(village.getDay() + 1);
         System.out.println("Day " + village.getDay() + " begins.");
-    }
-
-    private void talkToScholar() {
-        do {
-            System.out.println(SCHOLAR_MENU);
-            int menuChoice = scanner.nextInt();
-            switch (menuChoice) {
-                case 1:
-                    System.out.println("I am mute right now");
-                    break;
-                case 2:
-                    System.out.println("Binary -> Decimal");
-                    binaryToDecimal();
-                    break;
-                case 3:
-                    System.out.println("Decimal -> Binary");
-                    decimalToBinary();
-                    break;
-                case 4:
-                    System.out.println("Ask Scholar:");
-                    break;
-                case 5:
-                    return;
-            }
-        }while (isRunning);
-    }
-
-    private void binaryToDecimal() {
-        String input = scanner.next();
-        int result = input.charAt(0) - '0';
-        for(int i = 1; i < input.length(); i++){
-            int inputNumber = (int)input.charAt(i) - '0';
-            result = (result * 2  + inputNumber);
-        }
-        System.out.println(
-                "Calculating..." +
-                "\nBinary: " + input +
-                "\nDecimal: " + result + "\n");
-    }
-
-    private void decimalToBinary() {
-        int input = scanner.nextInt();
-        int decimalInput = input;
-        StringBuilder builder = new StringBuilder();
-        ArrayList<Integer> binaries = new ArrayList<>();
-        if(input == 0){
-            System.out.println(
-                    """
-                            Calculating...
-                            Binary: 0
-                            Decimal: 0
-                            """);
-            return;
-        }
-        else {
-            while (input != 0) {
-                binaries.add(input % 2);
-                input /= 2;
-            }
-        }
-        for(int i = binaries.size(); i > 0; i--) {
-            builder.append(binaries.get(i-1));
-        }
-        System.out.println(
-                "Calculating...\n" +
-                "Decimal: " + decimalInput + "\n" +
-                "Binary: " + builder + "\n");
     }
 
     private void quit() {
